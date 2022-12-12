b30c551af1a3b326ca1fbd6c03f854e88670ef0b
LEv145 <you@example.com>
2022-12-12 18:45:06 +0200

    v 0.2.10
    Fix hikari in `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,18 +1,46 @@
 {
-  description = "Cat";
+  description = "My Python application";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {self, nixpkgs, flake-utils}:
-    flake-utils.lib.eachDefaultSystem(system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        poetryOverrides = self: super: {
+          dataconf = super.dataconf.overridePythonAttrs(old: {
+            buildInputs = with super; [poetry];
+          });
+          sqlalchemy2-stubs = super.sqlalchemy2-stubs.overridePythonAttrs(old: {
+            buildInputs = with super; [setuptools poetry];
+          });
+          async-scheduler-object = super.async-scheduler-object.overridePythonAttrs(old: {
+            buildInputs = with super; [poetry];
+          });
+          hikari = super.hikari.overridePythonAttrs(old: {
+            src = pkgs.fetchFromGitHub {   # FIXME: https://github.com/hikari-py/hikari/issues/1399
+              repo = "hikari";
+              owner = "hikari-py";
+              rev = old.version;
+              hash = "sha256-vp3ujhXJEcaQnwC7Cn4Dn5FXGPTrEaHVC2S7GIfjno4=";
+            };
+            buildInputs = with super; [setuptools];
+          });
+        };
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides = [pkgs.poetry2nix.defaultPoetryOverrides poetryOverrides];
+        };
+        packageName = "discord-top-role-bot";
       in {
-        devShells.default = pkgs.mkShell rec {
-          buildInputs = with pkgs; [stdenv.cc.cc.lib];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        packages.${packageName} = app;
+        defaultPackage = self.packages.${system}.${packageName};
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [poetry];
+          inputsFrom = builtins.attrValues self.packages.${system};
         };
       }
     );
